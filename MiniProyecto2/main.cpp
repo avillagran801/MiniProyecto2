f26cd5d8d04412d72a428ef213cec4d0dba75429
Pepancho <joscortes2021@udec.cl>
2023-06-24 01:57:05 -0400

    Experiment update
    Se añade el código usado para la experimentación. Notar que está dividido en sectores y solo hay que comentarlos de no ser ocupados.

--- MiniProyecto2/main.cpp
+++ MiniProyecto2/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <chrono>
 #include "QuadTree.h"
 using namespace std;
 
@@ -60,20 +61,110 @@ vector<Data> readDataBase(int numLines) {
 }
 
 int main() {
-	int numData = 100;
-	vector<Data> dataBase = readDataBase(numData);
-	int width = 90;
+	// ---------------- IMPLEMENTACIÓN -------------------------------
+	
+	/*
+	// cout << "How many cities do you want to insert into the QuadTree?" << endl;
+	int numData;
+	// cin >> numData;
 
-	// El área más pequeña será de width longitude
+	// Leemos numData líneas de la base de datos (sin contar títulos) latitude las guardamos en el vector de Data
+	vector<Data> dataBase = readDataBase(numData);
+
+	cout << "What is the width of the smallest square? (from 0.0001 to 360)" << endl;
+	int width;
+	cin >> width;
+
+	// El área más pequeña será de width longitude (width/2)
 	QuadTree qt(width);
 
-	for (int i = 0; i < dataBase.size(); i++) {
+	for (int i = 0; i < numData; i++) {
 		qt.insert(dataBase[i].position, dataBase[i].city, dataBase[i].population);
 	}
 
-	cout << "Count region with p=(-85, 40) and d=1: " << qt.countRegion(Point(-85, 40), 1) << endl;
-
 	qt.printQuadTree();
+	*/
+
+	// ------------------ EXPERIMENTACIÓN ---------------------------
+
+	// insert()
+
+	/*
 	
+	int numData;
+	numData = 100000; // Cantidad de ciudades. 3.173.647; 1,6 m; 800 k; 400k; 200k; 100k
+	vector<Data> dataBase = readDataBase(numData);
+	double width;
+	width = 22.5; // La partición más pequeña. 11,25; 22,5; 45; 90; 180; 360
+	QuadTree qt(width);
+
+	int reps = 20;
+	double duration = 0;
+	for (int i = 0; i < reps; i++) {
+
+		auto start = chrono::high_resolution_clock::now();
+		//####################INICIO DEL CLOCK####################
+		for (int i = 0; i < numData; i++) {
+			qt.insert(dataBase[i].position, dataBase[i].city, dataBase[i].population);
+		}
+		//#####################FIN DEL CLOCK######################
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> diff = end - start;
+		duration += diff.count();
+	}
+	duration /= reps;
+
+	cout << "Se demoro " << duration << endl;
+
+	*/
+
+	// aggregateRegion() y countRegion()
+
+	
+
+	int numData;
+	numData = 3173647; // Cantidad de ciudades. 3.173.647
+	vector<Data> dataBase = readDataBase(numData);
+	double width;
+	width = 5.625; // La partición más pequeña. 0,703125; 1,40625; 2,8125; 5,625
+	int distance = 512; // La distancia que se tendrá en cuenta. 2; 100; 128; 256; 512
+	QuadTree qt(width);
+	for (int i = 0; i < numData; i++) {
+		qt.insert(dataBase[i].position, dataBase[i].city, dataBase[i].population);
+	}
+
+	int reps = 20;
+	double duration = 0;
+	for (int i = 0; i < reps; i++) {
+
+		auto start = chrono::high_resolution_clock::now();
+		//####################INICIO DEL CLOCK####################
+		qt.aggregateRegion(Point(0, 0), distance);
+		//#####################FIN DEL CLOCK######################
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> diff = end - start;
+		duration += diff.count();
+	}
+	duration /= reps;
+
+	cout << "Aggregate se demoro " << duration << endl;
+
+	duration = 0;
+	for (int i = 0; i < reps; i++) {
+
+		auto start = chrono::high_resolution_clock::now();
+		//####################INICIO DEL CLOCK####################
+		qt.countRegion(Point(0, 0), distance);
+		//#####################FIN DEL CLOCK######################
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> diff = end - start;
+		duration += diff.count();
+	}
+	duration /= reps;
+
+	cout << "Count se demoro " << duration << endl;
+
+	
+
 	return 0;
 }
